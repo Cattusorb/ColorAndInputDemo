e6ebb4dc21dfeef9f5c3d796248638bc6466da94
Cattusorb <magicmachineplaymc@gmail.com>
2019-02-06 09:21:43 -0500

    Added a TextView, EditText, and Button. Gave them functions and linked them together so they do what they are suppose to.

--- app/src/main/java/com/example/colorandinputdemo/MainActivity.java
+++ app/src/main/java/com/example/colorandinputdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.colorandinputdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Grab the text from the EditText and put it in the output
+     * @param view
+     */
+    public void submit(View view) {
+        EditText input = findViewById(R.id.input);
+        String inputText = input.getText().toString();
+
+        TextView display = findViewById(R.id.output);
+        display.setText(inputText);
     }
 }
